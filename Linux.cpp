0d359aa5909399170f0738084fc010f5b7683185
Gateway58 <73712423+Gateway58@users.noreply.github.com>
2021-05-03 19:37:54 +0200

    Update Linux.cpp

--- Linux.cpp
+++ Linux.cpp
@@ -1,1 +1,12 @@
+#include <include.h>
 
+using namespace std; 
+
+int main()
+{
+    for (int i = 0; i < 36; i++)
+    {
+       system("firefox -new-tab https://www.ecosia.org/search?q=new+shirt --private"); 
+    }
+    
+}
