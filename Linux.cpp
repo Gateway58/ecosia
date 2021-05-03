a751bc3e1a78e4bfe2a7b863bc25ec6a798bca57
Gateway58 <73712423+Gateway58@users.noreply.github.com>
2021-05-03 19:38:17 +0200

    Update Linux.cpp

--- Linux.cpp
+++ Linux.cpp
@@ -1,4 +1,16 @@
-#include <include.h>
+#include <iostream>
+#include <math.h>
+#include <fstab.h>
+#include <fstream>
+#include <ostream>
+#include <sstream>
+#include <condition_variable>
+#include <cerrno>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std; 
 
