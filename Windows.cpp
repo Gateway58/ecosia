9dbda36bc43bd89bf501ac810e7d04311b2142ba
Gateway58 <73712423+Gateway58@users.noreply.github.com>
2021-05-03 19:37:12 +0200

    Update Windows.cpp

--- Windows.cpp
+++ Windows.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <math.h>
+#include <fstab.h>
+#include <fstream>
+#include <ostream>
+#include <sstream>
+#include <condition_variable>
+#include <cerrno>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
 
+
+using namespace std; 
+
+int main()
+{
+    for (int i = 0; i < 35; i++)
+    {
+        system("start firefox -new-tab https://www.ecosia.org/search?q=new+shirt --private");
+    }
+    
+}
